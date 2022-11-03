e0f2f90d01d37791ea86bd0bb4fa9268f16ebcc0
Jessika Linares <jekalinares1225@gmail.com>
2022-11-02 23:07:50 -0500

    Cambios Admi

--- src/componentes/ProductsAdm.js
+++ src/componentes/ProductsAdm.js
@@ -15,35 +15,64 @@ export function ProductsAdm() {
   return (
      <Table striped bordered hover>
        <thead>
-         <tr>
-           <th>#</th>
-           <th>First Name</th>
-           <th>Last Name</th>
-           <th>Username</th>
-         </tr>
-       </thead>
-       <tbody>
-         <tr>
-           <td>1</td>
-           <td>Mark</td>
-           <td>Otto</td>
-           <td>@mdo</td>
-         </tr>
-         <tr>
-           <td>2</td>
-           <td>Jacob</td>
-           <td>Thornton</td>
-           <td>@fat</td>
-         </tr>
-         <tr>
-           <td>3</td>
-           <td colSpan={2}>Larry the Bird</td>
-           <td>@twitter</td>
-         </tr>
-       </tbody>
-     </Table>
-    
- 
-    
+       <tr>
+          <th>#</th>
+          <th>Producto</th>
+          <th>Descripción</th>
+          <th>Cantidad</th>
+          <th>Precio</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Disfraz</td>
+          <td>disfraz en algodón </td>
+          <td>80</td>
+          <td>25.000</td>
+          <button type="button" class="btn btn-dark" align="center">Modificar</button>
+        </tr>
+        <tr>
+        <td>2</td>
+          <td>Blin-Blind</td>
+          <td>Camisetas algodon estilo deporte americano</td>
+          <td>90</td>
+          <td>35.000</td>
+          <button type="button" class="btn btn-dark" align="center">Modificar</button>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Saco capota</td>
+          <td>Saco de capota gris algodón perchado</td>
+          <td>50</td>
+          <td>45.000</td>
+          <button type="button" class="btn btn-dark" align="center">Modificar</button>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Gorro Gato (cerdito)</td>
+          <td>Gorro de algodón con motivo de cerdito</td>
+          <td>50</td>
+          <td>15.000</td>
+          <button type="button" class="btn btn-dark" align="center">Modificar</button>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Saco tejido Gato</td>
+          <td>saco tejido de lana sintetica para gato</td>
+          <td>50</td>
+          <td>28.000</td>
+          <button type="button" class="btn btn-dark" align="center">Modificar</button>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>Saco capota</td>
+          <td>saco de capota verde para gato</td>
+          <td>30</td>
+          <td>25.000</td>
+          <button type="button" class="btn btn-dark" align="center">Modificar</button>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
