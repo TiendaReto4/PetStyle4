72e4fdaace2fa790ef8ef28ee0e328e4904fc106
molinaciclo3 <molinamath50@gmail.com>
2022-11-03 00:23:13 -0500

    actualizado vista ventas

--- src/componentes/VentasAdm.js
+++ src/componentes/VentasAdm.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Table from 'react-bootstrap/Table';
 
 export function VentasAdm() {
-  return <h1>tabla de ventas vista del adm</h1>;
+  return (
+    <Table striped bordered hover>
+       <thead>
+         <tr>
+           <th>#</th>
+           <th>Fecha</th>
+           <th>Nombre producto</th>
+           <th>Cantidad</th>
+           <th>Precio</th>
+           <th>Total</th>
+         </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <td>1</td>
+           <td>10/01/2022</td>
+           <td>disfraz</td>
+           <td>1</td>
+           <td>25000</td>
+           <td>25000</td>
+         </tr>
+         <tr>
+           <td>2</td>
+           <td>11/01/2022</td>
+           <td>Saco capota</td>
+           <td>1</td>
+           <td>45000</td>
+           <td>45000</td>
+         </tr>
+        
+       </tbody>
+     </Table>
+  )
 
   //tabla de inventario para ver los productos exitentes crear las columnas requeridas 
 }
